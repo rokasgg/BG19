366450f8c7a35b0543623ce5fa4370fcb818c0bd
rokasgg <rokas.gegzna@gmail.com>
2019-11-25 20:22:58 +0200

    second commit

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import Router from "./navigation/router";
 import store from "./redux/store";
-
+//Zdrastvite
 class App extends React.Component {
   render() {
     return (
